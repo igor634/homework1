a193d07f30d14d69b7c986e4279b006ca9d1b004
igor634 <darienko.igor2017@gmail.com>
2024-09-08 16:38:51 +0300

    Добавьте файлы проекта.

---
+++ homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Назовите ваше имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"{name}");
+            Console.Write("Назовите ваш возраст: ");
+            var age = Console.ReadLine();
+            Console.WriteLine($"{age}");
+            Console.Write("Назовите ваш вес: ");
+            var weight = Console.ReadLine();
+            Console.WriteLine($"{weight}");
+            Console.WriteLine($"{name},{age} лет,вес:{weight}");
+        }
+    }
+}
