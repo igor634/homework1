6a75ce00bd4204dda868bbd2ab04e8fda23c00f8
igor634 <darienko.igor2017@gmail.com>
2024-09-08 18:20:09 +0300

    Program.cs

--- Homework1(b)/Homework1(b)/Program.cs
+++ Homework1(b)/Homework1(b)/Program.cs
@@ -4,22 +4,16 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Введите число: ");
-
-            //float f1 = Convert.ToSingle(Console.ReadLine());
-            //Console.WriteLine("Введите второе число: ");
-            //float f2 = Convert.ToSingle(Console.ReadLine());
-            //Console.WriteLine(String.Format("{0:F2}", $"{f1} + {f2} =  {f1 + f2}"));
             Console.Write("Введите первое число: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите второе число: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            // Вычисляем сумму
+            
             double sum = num1 + num2;
 
-            // Выводим результат с округлением до двух знаков после запятой
+        
             Console.WriteLine("Сумма: {0:F2}", sum);
         }
     }
