40997d9821ab14e6acc8c30e80ba6e4587dd05ec
igor634 <darienko.igor2017@gmail.com>
2024-09-13 20:53:53 +0300

    homework2 with year

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            
+            
+
+            if ((a1 % 4 == 0 && a1 % 100 != 0) || (a1 % 400 == 0))
+                Console.WriteLine($"{a1} -  високосный");
+            else
+                Console.WriteLine($"{a1} - невисокосный");
+        }
+    }
+}
