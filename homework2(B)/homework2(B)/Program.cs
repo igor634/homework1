3cfc38eca2ebb5951888e5804cc2215798e051e6
igor634 <darienko.igor2017@gmail.com>
2024-09-13 21:43:54 +0300

    homework2 random numbers

---
+++ homework2(B)/homework2(B)/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace homework2_B_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                
+                int secretNumber = random.Next(1, 15);
+                int attempts = 5;  
+                bool numberGuessed = false;
+
+                Console.WriteLine("Компьютер загадал число от 1 до 15. Попробуйте угадать!");
+
+                
+                while (attempts > 0 && !numberGuessed)
+                {
+                    Console.Write($"У вас {attempts} попыток. Введите ваше число: ");
+                    int guess;
+
+                    
+                    if (int.TryParse(Console.ReadLine(), out guess))
+                    {
+                        if (guess == secretNumber)
+                        {
+                            Console.WriteLine("Поздравляем! Вы угадали число!");
+                            numberGuessed = true;
+                        }
+                        else if (guess > secretNumber)
+                        {
+                            Console.WriteLine("Загаданное число меньше.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Загаданное число больше.");
+                        }
+
+                        attempts--;  
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите корректное число!");
+                    }
+                }
+
+                if (!numberGuessed)
+                {
+                    Console.WriteLine($"Вы проиграли! Загаданное число было: {secretNumber}");
+                }
+
+                
+                Console.Write("Хотите сыграть ещё раз? (y/n): ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response != "y")
+                {
+                    playAgain = false;
+                    Console.WriteLine("Спасибо за игру!");
+                }
+            }
+        }  
+    }
+}
