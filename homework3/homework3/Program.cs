bc783d4f9c975bb0e9c88929c3763ef599ab5dd0
igor634 <darienko.igor2017@gmail.com>
2024-09-17 17:20:11 +0300

    homework 3 with massiv

---
+++ homework3/homework3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+
+namespace homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int[] m1 = { 79, 597, 1, 29, -5};
+            Console.WriteLine("введите число: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            if (a1 == 79)
+            {
+                Console.WriteLine("1");
+            }
+            else if (a1 == 597)
+            {
+                Console.WriteLine("0");
+            }
+            else if (a1 == 1)
+            {
+                Console.WriteLine("3");
+            }
+            else if (a1 == 29)
+            {
+                Console.WriteLine("2");
+            }
+            else if (a1 == -5)
+            {
+                Console.WriteLine("у вас отрицательное число поэтому посчитаем все положительные: ");
+                Console.WriteLine(m1[0]);
+                Console.WriteLine(m1[1]);
+                Console.WriteLine(m1[2]);
+                Console.WriteLine(m1[3]);
+            }
+            
+        }
+    }
+}
