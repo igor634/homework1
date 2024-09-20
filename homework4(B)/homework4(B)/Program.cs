7b311e1720294d64a30b303048e5710cfe0ba8e2
igor634 <darienko.igor2017@gmail.com>
2024-09-20 15:09:35 +0300

    method рекурсия

---
+++ homework4(B)/homework4(B)/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace homework4_B_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+
+        }
+        int factorial(int a1)
+        {
+            if (a1 == 1);
+
+            return a1 * factorial(a1 - 1);
+            Console.WriteLine(factorial(a1));
+        }
+        
+
+    }
+}
