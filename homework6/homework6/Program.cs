45589a8392ea575af5271ed03ce5a94d2ffde06b
igor634 <darienko.igor2017@gmail.com>
2024-09-28 10:44:35 +0300

    homework with dictionary

---
+++ homework6/homework6/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace homework6
+{
+    internal class Program
+    {
+        class WordCounter
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите строку текста:");
+                string input = Console.ReadLine();
+
+                // Убираем знаки препинания и приводим текст к нижнему регистру
+                string[] words = input.ToLower()
+                .Split(new char[] { ' ', '.', ',', '!', '?', ':', ';', '-', '(', ')', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                
+                Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+               
+                foreach (string word in words)
+                {
+                    if (wordCounts.ContainsKey(word))
+                    {
+                        // Если слово уже есть в словаре, увеличиваем счётчик
+                        wordCounts[word]++;
+                    }
+                    else
+                    {
+                        // Если слова ещё нет в словаре, добавляем его с начальным счётчиком 1
+                        wordCounts.Add(word, 1);
+                    }
+                }
+
+                
+                Console.WriteLine("Количество повторений каждого слова:");
+                foreach (var pair in wordCounts)
+                {
+                    Console.WriteLine($"Слово '{pair.Key}' встречается {pair.Value} раз(а)");
+                }
+            }
+        }
+    }
+}
