d21e287cd0824544a19a606aa2b8eea2a3dc8656
igor634 <darienko.igor2017@gmail.com>
2024-12-07 11:30:42 +0200

    homework21

---
+++ homework21(client)/homework21(client)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public Order(int id, string productName, int quantity, DateTime orderDate)
+    {
+        Id = id;
+        ProductName = productName;
+        Quantity = quantity;
+        OrderDate = orderDate;
+    }
+}
+
+public class Client
+{
+    public static void Main()
+    {
+        string serverAddress = "127.0.0.1";  // Адрес сервера
+        int port = 8080;
+
+        // Создаем заказ
+        Order order = new Order(1, "Продукт 1", 5, DateTime.Now);
+        string orderData = $"{order.Id},{order.ProductName},{order.Quantity},{order.OrderDate}";
+
+        TcpClient client = new TcpClient(serverAddress, port);
+        NetworkStream stream = client.GetStream();
+
+        // Отправляем заказ
+        byte[] data = Encoding.UTF8.GetBytes(orderData);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Заказ отправлен на сервер.");
+
+        // Получаем ответ от сервера
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Ответ от сервера: " + response);
+
+        client.Close();
+    }
+}
---
+++ homework21/homework21/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public Order(int id, string productName, int quantity, DateTime orderDate)
+    {
+        Id = id;
+        ProductName = productName;
+        Quantity = quantity;
+        OrderDate = orderDate;
+    }
+}
+
+public class Server
+{
+    private static List<Order> orders = new List<Order>();  // Список заказов
+
+    public static void Main()
+    {
+        int port = 8080;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Сервер ожидает подключения...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Клиент подключился.");
+            NetworkStream stream = client.GetStream();
+
+            // Чтение данных от клиента
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string orderData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            // Десериализация данных в объект Order (предположим, что данные передаются в виде строки)
+            string[] data = orderData.Split(',');
+            int id = int.Parse(data[0]);
+            string productName = data[1];
+            int quantity = int.Parse(data[2]);
+            DateTime orderDate = DateTime.Parse(data[3]);
+
+            Order order = new Order(id, productName, quantity, orderDate);
+            orders.Add(order);
+
+            // Отправка подтверждения клиенту
+            string responseMessage = $"Заказ №{id} принят.";
+            byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+            stream.Write(responseData, 0, responseData.Length);
+
+            client.Close();
+        }
+    }
+}
